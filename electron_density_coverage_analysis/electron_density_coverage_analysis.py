3f057216b2f14d3dbde43bd836fb29cbb623d57d
Viki Doshchenko <42894967+victoria-dos@users.noreply.github.com>
2024-04-18 16:31:12 +0200

    Add logging to density analysis

--- electron_density_coverage_analysis/electron_density_coverage_analysis.py
+++ electron_density_coverage_analysis/electron_density_coverage_analysis.py
@@ -4,8 +4,10 @@ import gemmi
 import statistics as st
 import math
 
+
 def run_as_function(args: argparse.Namespace):
     return run_analysis(args)
+
 
 # compare intensity, corresponding to the given position, to the threshold for isosurface (MORE vs MORE OR EQUAL)
 def determine_atom_coverage(pos, map, sigma_lvl, args: argparse.Namespace):
@@ -16,6 +18,7 @@ def determine_atom_coverage(pos, map, sigma_lvl, args: argparse.Namespace):
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
 
+
 # get intensity corresponding to the given position (trilinear interpolation vs itensity of the closest voxel)
 def get_intensity(pos, map, args: argparse.Namespace):
     try:
@@ -24,6 +27,7 @@ def get_intensity(pos, map, args: argparse.Namespace):
         return map.grid.interpolate_value(pos)
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
+
 
 def run_analysis(args: argparse.Namespace):
     try:
@@ -53,7 +57,6 @@ def run_analysis(args: argparse.Namespace):
                             if determine_atom_coverage(atom.pos, map, sigma_lvl, args):
                                 covered_atoms_count = covered_atoms_count + 1
 
-            
             output = f'{covered_atoms_count};{total_atom_count}'
             # print(covered_atoms_count, total_atom_count, sep=";", end='')
 
@@ -74,25 +77,48 @@ def run_analysis(args: argparse.Namespace):
 
     return output
 
-def main():
-    parser = argparse.ArgumentParser(description="Determines electron density coverage of a cycle. By default, uses trilinear interpolation to infer the electron density value, and considers an atom to be covered by the electron density when the corresponding intensity is MORE than the threshold for the isosurface (1.5 sigma)")
-    parser.add_argument('-s', action='store_true', help='Simple mode - output is two numbers: first is the number of covered atoms, the second is the total number of atoms in a cycle')
-    parser.add_argument('-d', action='store_true', help='Detailed mode - output a sequence of paried values, where the first member of each pair is a serial atom number, and the second member is "y" - if atom is covered, and "n" - if it is not.')
-    parser.add_argument('-m', '--more_or_equal', action='store_true', help='Atom is considered to be covered by the electron density when the corresponding intensity is MORE OR EQUAL to the threshold for the isosurface')
-    parser.add_argument('-c', '--closest_voxel', action='store_true', help='Instead of trilinear interpolation, the intensity of the closest voxel is used')
 
-    parser.add_argument('input_cycle_pdb', type=str, help='Input PDB file with coordinates of a cycle, produced by PatternQuery')
-    parser.add_argument('input_density_ccp4', type=str, help='Input electron density file for the corresponding protein structure from the PDB in CCP4 format')
-
-    args = parser.parse_args()
-    # print(args)
-    output = run_analysis(args)
-    return output
-
-if __name__ == '__main__':
-    output = main()
-    if isinstance(output, str):
-        print(output)
-    elif isinstance(output, list):
-        for i in output:
-            print(i)
+# def main():
+#     parser = argparse.ArgumentParser(description="Determines electron density coverage of a cycle. By default, "
+#                                                  "uses trilinear interpolation to infer the electron density value, "
+#                                                  "and considers an atom to be covered by the electron density when "
+#                                                  "the corresponding intensity is MORE than the threshold for the "
+#                                                  "isosurface (1.5 sigma)")
+#     parser.add_argument('-s',
+#                         action='store_true', help='Simple mode - output is two numbers: first is the number of '
+#                                                   'covered atoms, the second is the total number of atoms in a '
+#                                                   'cycle')
+#     parser.add_argument('-d',
+#                         action='store_true', help='Detailed mode - output a sequence of paried values, where '
+#                                                   'the first member of each pair is a serial atom number, '
+#                                                   'and the second member is "y" - if atom is covered, '
+#                                                   'and "n" - if it is not.')
+#     parser.add_argument('-m', '--more_or_equal',
+#                         action='store_true', help='Atom is considered to be covered by the '
+#                                                   'electron density when the corresponding '
+#                                                   'intensity is MORE OR EQUAL to the '
+#                                                   'threshold for the isosurface')
+#     parser.add_argument('-c', '--closest_voxel',
+#                         action='store_true', help='Instead of trilinear interpolation, the '
+#                                                   'intensity of the closest voxel is used')
+#
+#     parser.add_argument('input_cycle_pdb',
+#                         type=str, help='Input PDB file with coordinates of a cycle, produced by '
+#                                        'PatternQuery')
+#     parser.add_argument('input_density_ccp4',
+#                         type=str, help='Input electron density file for the corresponding protein structure from the '
+#                                        'PDB in CCP4 format')
+#
+#     args = parser.parse_args()
+#     print(f"Arguments in electron densicty script are: {args} ")
+#     output = run_analysis(args)
+#     return output
+#
+#
+# if __name__ == '__main__':
+#     output = main()
+#     if isinstance(output, str):
+#         print(output)
+#     elif isinstance(output, list):
+#         for i in output:
+#             print(i)
--- electron_density_coverage_analysis/main.py
+++ electron_density_coverage_analysis/main.py
@@ -1,8 +1,7 @@
 import csv
 import logging
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Pipe, Process
 import os
-import urllib.request as r
 import argparse
 from pathlib import Path
 import shutil
@@ -14,6 +13,15 @@ EXE = Path('./electron_density_coverage_analysis.py')
 OUTPUT_DIR = Path('./output')
 CPU_COUNT = cpu_count() / 2
 
+
+def print_messages(conn):
+    while True:
+        message = conn.recv()
+        if message == 'STOP':
+            break
+        print(message)
+
+
 def _create_output_folder():
     try:
         if OUTPUT_DIR.exists():
@@ -24,13 +32,6 @@ def _create_output_folder():
         logging.error(e, stack_info=True, exc_info=True)
     
     return OUTPUT_DIR
-
-def _download_single_file(data: tuple):
-    link, path, pdb_id = data
-    try:
-        r.urlretrieve(link, path)
-    except:
-        return pdb_id
 
 
 def process_args(args: argparse.Namespace):
@@ -49,6 +50,7 @@ def process_args(args: argparse.Namespace):
 
     return a
 
+
 def run_exe(ligand_filepath: Path, arguments: argparse.Namespace):
     try:
         PQ_pdb_name = ligand_filepath.name.split(".")[0]
@@ -58,11 +60,16 @@ def run_exe(ligand_filepath: Path, arguments: argparse.Namespace):
         
         arguments.input_cycle_pdb = str(ligand_filepath.resolve())
         arguments.input_density_ccp4 = ccp4_filepath
+
+        # conn.send(f"Processing {ligand_filepath}")
         output = run_as_function(arguments)
         result = (PQ_pdb_name, residue_id, output)
+        # conn.send(f"Finished processing {ligand_filepath}")
+
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
     return result
+
 
 def get_filepathes(rootdir: Path, ring_type: str):
     try:
@@ -70,16 +77,20 @@ def get_filepathes(rootdir: Path, ring_type: str):
 
         all_ccp4_files = CCP4_DIR.glob('**/*')
         pdb_ids_for_which_ccp4_is_available = [x.stem for x in all_ccp4_files]
-        for f in Path(rootdir / ring_type / 'filtered_ligands').rglob("*"):
+        for f in Path(rootdir / 'validation_data' / ring_type / 'filtered_ligands').rglob("*"):
             if f.is_file():
                 # get path
                 stem = f.stem
                 pdb_id = stem.split('_')[1]
                 if pdb_id in pdb_ids_for_which_ccp4_is_available:
+                    # logging.info(f"Getting file {f.stem}")
                     l.append(f)
+        logging.info(f"[{ring_type.capitalize()}]: There are {len(l)} PDB structures with corresponding CCP4 file "
+                     f"available.")
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
     return l
+
 
 def run_analysis(args: argparse.Namespace):
     try:
@@ -91,31 +102,68 @@ def run_analysis(args: argparse.Namespace):
             params = params + "m"
 
         ring_types = ['cyclohexane', 'cyclopentane', 'benzene']
+
+        # # Create a Pipe for communication between processes
+        # parent_conn, child_conn = Pipe()
+        #
+        # # Start the printing process
+        # printing_process = Process(target=print_messages, args=(child_conn,))
+        # printing_process.start()
+
         for ring_type in ring_types:
+
+            filepaths = get_filepathes(Path(args.rootdir), ring_type)
+            if len(filepaths) == 0:
+                logging.info(f"No files for analysis found for ring {ring_type}")
+                continue
+
             cvs_filename = ring_type + '_params_' + params + '_analysis_output.csv'
             with open(str(Path(OUTPUT_DIR / cvs_filename).resolve()), mode='w', newline='') as f:
-                rows = []
+                # rows = []
                 w = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                filepathes = get_filepathes(Path(args.rootdir), ring_type)
-      
+
                 with Pool(int(CPU_COUNT)) as p:
-                    modified_filepathes = [(f, arguments) for f in filepathes]
-                    rows = p.starmap(run_exe, modified_filepathes)
+                    modified_filepaths = [(f, arguments) for f in filepaths]
+                    logging.info(f"[{ring_type.capitalize()}]: Starting analysis for {len(filepaths)} files...")
+                    rows = p.starmap(run_exe, modified_filepaths)
                     w.writerows(rows)
+                    logging.info(f"[{ring_type.capitalize()}]: Finished analysis for {len(filepaths)} files.")
+
+        # # Signal the printing process to stop
+        # parent_conn.send('STOP')
+        # printing_process.join()
     except Exception as e:
         logging.error(e, stack_info=True, exc_info=True)
 
+
 def main():
     parser = argparse.ArgumentParser(description='ED coverage analysis')
-    parser.add_argument('rootdir', type=str, help='Root directory (e.g. "validation_data")')
-    
-    parser.add_argument('-s', action='store_true', help='Simple mode - output is two numbers: first is the number of covered atoms, the second is the total number of atoms in a cycle')
-    parser.add_argument('-m', '--more_or_equal', action='store_true', help='Atom is considered to be covered by the electron density when the corresponding intensity is MORE OR EQUAL to the threshold for the isosurface')
-    parser.add_argument('-c', '--closest_voxel', action='store_true', help='Instead of trilinear interpolation, the intensity of the closest voxel is used')
+    parser.add_argument('rootdir', type=str,
+                        help='Root directory of the result data (<ROOTDIR>/validation_data/etc)')
+    # parser.add_argument('ccp4_dir',
+    #                     type=str, help='Directory with ccp4 files')
+
+    parser.add_argument('-s',
+                        action='store_true', help='Simple mode - output is two numbers: first is the number of '
+                                                  'covered atoms, the second is the total number of atoms in a cycle')
+    parser.add_argument('-m', '--more_or_equal',
+                        action='store_true', help='Atom is considered to be covered by the electron density when the '
+                                                  'corresponding intensity is MORE OR EQUAL to the threshold for the '
+                                                  'isosurface')
+    parser.add_argument('-c', '--closest_voxel',
+                        action='store_true', help='Instead of trilinear interpolation, the intensity of the closest '
+                                                  'voxel is used')
     
     args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename='density-coverage.log')
+    logging.info(f"Running electron density coverage analysis on CPU count: {CPU_COUNT}")
+
+    print(f"Arguments of main.py are: {args} ")
     _create_output_folder()
     run_analysis(args)
+
 
 if __name__ == '__main__':
     main()
