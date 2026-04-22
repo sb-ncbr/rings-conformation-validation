560f72343caf5b266252a434ba96b902e182ddeb
victoria <doshchenko.victoria@gmail.com>
2026-04-22 20:25:47 +0200

    rewrite filter to include 5chars ccd ids

--- FilterDataset.py
+++ FilterDataset.py
@@ -2,101 +2,153 @@ import logging
 import os
 import sys
 import shutil
+import pandas as pd
+import pickle
+from pathlib import Path
 from argparse import ArgumentParser
 from gemmi import cif
 from HelperModule.Ring import Ring
-from HelperModule.getter_functions import get_data_from_pdb, get_bonds_from_cif
+from HelperModule.getter_functions import (
+    get_bonds_from_cif,
+    get_atoms_from_pdb,
+)
 from HelperModule.helper_functions import are_bonds_correct
 from HelperModule.constants import *
 
 
-def run_filter(input_dir: str, ring: Ring, output_dir: str, document: cif.Document) -> None:
-    total = 0
+def process_correct_rings(output_dir, ligand, filepath):
+    output_pdb_dir = output_dir / ligand / "patterns"
+
+    output_pdb_dir.mkdir(parents=True, exist_ok=True)
+    filename = filepath.name
+    if not filename.startswith(f"{ligand}_"):
+        filename = f"{ligand}_{filename}"
+
+    new_name_path = output_pdb_dir / filename
+    shutil.copy(filepath, new_name_path)
+
+
+def run_filter(
+    input_path: Path, ring: Ring, output_dir: Path, document: cif.Document
+) -> None:
+    patterns_df = pd.read_csv(input_path / "patterns.csv")
+    pkl_path = Path(f"{ring.name.lower()}_ligand_ring_map.pkl")
+    processed_data_dict = {}
+
+    if pkl_path.is_file():
+        with pkl_path.open("rb") as f:
+            processed_data_dict = pickle.load(f)
+
     target_count = 0
 
-    for root, _, files in os.walk(input_dir):
-        for file in files:
-            if not file.endswith('.pdb'):
+    for row in patterns_df.itertuples(index=False):
+        ligand = row.Residues.split()[0]
+
+        filepath = input_path / "patterns" / (row.Id + ".pdb")
+        atom_names = get_atoms_from_pdb(filepath, ring)
+
+        key = (ligand, frozenset(atom_names))
+
+        if key in processed_data_dict:
+            # skipping because already filtered out this ring as wrong
+            if not processed_data_dict[key]:
                 continue
-            total += 1
-            filepath = os.path.join(root, file)
-            ligand, atom_names = get_data_from_pdb(Path(filepath), ring)
-            ligand_block = document.find_block(ligand)
 
-            if ligand_block is None:
-                logging.warning(f"Ligand_block is None for {ligand}")
-                continue
+            process_correct_rings(output_dir, ligand, filepath)
+            target_count += 1
+            continue
 
-            atom_bonds = get_bonds_from_cif(ligand_block)
+        ligand_block = document.find_block(ligand)
+        if ligand_block is None:
+            logging.warning(f"Ligand_block is None for {ligand}")
+            continue
 
-            if are_bonds_correct(atom_names, atom_bonds, ring):
-                output_pdb_dir = os.path.join(output_dir, ligand, 'patterns')
-                os.makedirs(output_pdb_dir, exist_ok=True)
+        atom_bonds = get_bonds_from_cif(ligand_block)
+        is_correct = are_bonds_correct(atom_names, atom_bonds, ring)
+        processed_data_dict[key] = is_correct
 
-                # ligand name can be up to 3 chars
-                if os.path.basename(filepath)[len(ligand)] != '_':
-                    new_name_path = os.path.join(output_pdb_dir,
-                                                 ligand + '_' + os.path.basename(filepath))
-                else:
-                    new_name_path = os.path.join(output_pdb_dir, os.path.basename(filepath))
-                logging.info(f"Copying {os.path.basename(filepath)}")
-                shutil.copy(filepath, new_name_path)
-                target_count += 1
+        if is_correct:
+            process_correct_rings(output_dir, ligand, filepath)
+            target_count += 1
+
+    with pkl_path.open("wb") as f:
+        pickle.dump(processed_data_dict, f)
 
     logging.info(f"[{ring.name.capitalize()}]: {target_count} patterns were found.")
 
 
 def main(ring: str, output_path: str, input_path: str):
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s'
-                        )
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
 
     ring = ring.upper()
     if ring not in Ring.__members__.keys():
-        logging.error(f"Ring {ring} is not a valid Ring. Currently supported: {[e.name for e in Ring]} Exiting...")
+        logging.error(
+            f"Ring {ring} is not a valid Ring. Currently supported: {[e.name for e in Ring]} Exiting..."
+        )
         sys.exit(1)
 
     logging.info(f"[{ring.capitalize()}]: Starting FilterDataset...")
 
-    main_workflow_output_dir = os.path.join(output_path, MAIN_DIR)
+    main_workflow_output_dir = Path(output_path) / MAIN_DIR
     if not os.path.exists(main_workflow_output_dir):
-        logging.error(f"The directory {os.path.abspath(main_workflow_output_dir)} does not exist. Exiting...")
+        logging.error(
+            f"The directory {main_workflow_output_dir.resolve()} does not exist. Exiting..."
+        )
         sys.exit(1)
 
-    path_to_comp_dict = os.path.join(input_path, DEFAULT_DICT_NAME)
-    logging.info('Reading components dictionary...')
-    if not os.path.exists(path_to_comp_dict):
+    path_to_comp_dict = Path(input_path) / DEFAULT_DICT_NAME
+
+    if not path_to_comp_dict.exists():
         logging.error(f"The file {path_to_comp_dict} does not exist. Exiting...")
         sys.exit(1)
 
-    current_ring_path = os.path.join(main_workflow_output_dir, ring.lower())
-
     # that is the output dir from the previous script (previous step)
-    dir_with_patterns = os.path.join(main_workflow_output_dir, "result", ring.lower())
+    dir_with_patterns = main_workflow_output_dir / "result" / ring.lower()
 
-    if not os.path.exists(dir_with_patterns) or not os.listdir(dir_with_patterns):
+    if not dir_with_patterns.exists() or not any(dir_with_patterns.iterdir()):
         logging.error(f'The directory "{dir_with_patterns}" does not exist or is empty')
         sys.exit(1)
 
-    dir_for_filtered_patterns = os.path.join(current_ring_path, 'filtered_ligands')
+    current_ring_path = main_workflow_output_dir / ring.lower()
+    dir_for_filtered_patterns = current_ring_path / "filtered_ligands"
 
-    document = cif.read(path_to_comp_dict)
+    logging.info("Reading components dictionary...")
+    document = cif.read(str(path_to_comp_dict))
     run_filter(dir_with_patterns, Ring[ring], dir_for_filtered_patterns, document)
-    logging.info(f'[{ring.capitalize()}]: FilterDataset has completed successfully')
+    logging.info(f"[{ring.capitalize()}]: FilterDataset has completed successfully")
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description="Filter out the wrong patterns, which do not possess "
-                                        "the required atomic bonds")
-    required = parser.add_argument_group('required named arguments')
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Filter out the wrong patterns, which do not possess "
+        "the required atomic bonds"
+    )
+    required = parser.add_argument_group("required named arguments")
 
-    required.add_argument('-r', '--ring', required=True, type=str,
-                          help=f'Choose the target ring type. Currently supported:'
-                               f' {[e.name for e in Ring]}')
-    required.add_argument('-o', '--output', type=str, required=True,
-                          help='Path to the output directory. Should be the same as in the previous step.')
-    required.add_argument('-i', '--input', type=str, required=True,
-                          help='Path to the directory with input data (local pdb, ccp4 files, etc.)')
+    required.add_argument(
+        "-r",
+        "--ring",
+        required=True,
+        type=str,
+        help=f"Choose the target ring type. Currently supported:"
+        f" {[e.name for e in Ring]}",
+    )
+    required.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="Path to the output directory. Should be the same as in the previous step.",
+    )
+    required.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        required=True,
+        help="Path to the directory with input data (local pdb, ccp4 files, etc.)",
+    )
 
     args = parser.parse_args()
 
--- HelperModule/getter_functions.py
+++ HelperModule/getter_functions.py
@@ -1,17 +1,18 @@
+import pandas as pd
 from pathlib import Path
 from gemmi import cif
+from typing import Dict
 from HelperModule.Ring import Ring
 
 
-def get_data_from_pdb(path_to_pdb: Path, ring: Ring) -> tuple[str, list[str]]:
+def get_atoms_from_pdb(path_to_pdb: Path, ring: Ring) -> list[str]:
     atom_names = []
     with open(path_to_pdb, "r") as file:
         lines = file.read().splitlines()
-        ligand = lines[1][17:20].strip()
 
         for line in lines[1 : ring.atom_number + 1]:
             atom_names.append(line[12:16].strip())
-    return ligand, atom_names
+    return atom_names
 
 
 def get_bonds_from_cif(ligand_block: cif.Block) -> list[list[str]]:
@@ -26,3 +27,27 @@ def get_bonds_from_cif(ligand_block: cif.Block) -> list[list[str]]:
     )
 
     return [list(x) for x in list(table)]
+
+
+def get_data_from_cif(ligand_block: cif.Block) -> Dict[str, list[list[str]]]:
+
+    bond_table = ligand_block.find(
+        [
+            "_chem_comp_bond.atom_id_1",
+            "_chem_comp_bond.atom_id_2",
+            "_chem_comp_bond.value_order",
+            "_chem_comp_bond.pdbx_aromatic_flag",
+        ]
+    )
+
+    atom_table = ligand_block.find(
+        ["_chem_comp_atom.atom_id", "_chem_comp_atom.type_symbol"]
+    )
+
+    bond_df = pd.DataFrame(
+        bond_table, columns=["atom_id_1", "atom_id_2", "order", "aromatic"]
+    )
+
+    atom_df = pd.DataFrame(atom_table, columns=["atom_id", "type_symbol"])
+
+    return bond_df, atom_df
